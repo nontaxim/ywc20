5a07030a6e2931eb2a115e6a5da1238c5eb7d07c
Nontawat <mix_sk139@hotmail.com>
2025-05-11 11:00:42 +0700

    fix(announcement): add checker if no firstname or lastname redirect back

--- src/routes/announcement.tsx
+++ src/routes/announcement.tsx
@@ -8,6 +8,7 @@ import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card';
 import {Button} from '@/components/ui/button';
 import {getRandomCommitteeComment, getRandomFeedback} from '@/utils/comments';
 import {AnnouncementSkeleton} from '@/components/AnnouncementSkeleton';
+import {useEffect} from 'react';
 
 type UserSearch = z.infer<typeof userSearchSchema>;
 
@@ -30,6 +31,18 @@ function Announcement() {
         queryFn: getCandidates,
         refetchOnWindowFocus: false,
     });
+    useEffect(() => {
+        if (!firstName || !lastName) {
+            navigate({
+                to: '/',
+                search: {
+                    firstName: '',
+                    lastName: '',
+                    major: '',
+                },
+            });
+        }
+    }, [firstName, lastName, navigate]);
     const defaultMajor = 'design';
     if (isLoading) return <AnnouncementSkeleton />;
     if (isError)
@@ -60,7 +73,7 @@ function Announcement() {
     }
     return (
         <div className="flex h-full w-full flex-col items-center justify-center gap-2 px-4 sm:gap-4 sm:px-8">
-            <Card className="h-auto w-full rounded-lg bg-gradient-to-b from-black via-black to-[#190200] shadow-lg sm:w-3/4 lg:w-2/3">
+            <Card className="h-auto w-full gap-2 rounded-lg border-0 bg-gradient-to-b from-black via-black to-[#190200] shadow-lg sm:w-3/4 sm:gap-4 sm:border lg:w-2/3">
                 <CardHeader>
                     <CardTitle className="text-center text-2xl font-bold text-white">
                         {matched
@@ -68,49 +81,44 @@ function Announcement() {
                             : 'Regarding your Application to Young Webmaster Camp 20'}
                     </CardTitle>
                 </CardHeader>
-                <CardContent className="space-y-4 p-6 pt-0 text-center text-white">
+                <CardContent className="space-y-2 px-0 text-center text-white sm:space-y-4 sm:px-6">
                     <div className="text-xl font-semibold">
                         {firstName} {lastName}
                     </div>
 
                     {matched ? (
                         <>
-                            <div className="flex flex-col items-center justify-center text-center text-green-400">
+                            <div className="flex flex-col items-center justify-center text-center">
                                 <div className="mb-2 animate-bounce text-4xl">
                                     🎉
                                 </div>
-                                <div className="text-lg font-semibold sm:text-xl">
+                                <div className="bg-[linear-gradient(90deg,_#f81a64_0%,_#f52222_50%,_#ff691d_86%,_#ffb623)] bg-clip-text text-lg font-semibold text-transparent sm:text-xl">
                                     You've been{' '}
-                                    <span className="text-green-300">
-                                        selected
-                                    </span>{' '}
-                                    in
+                                    <span className="">selected</span> in
                                 </div>
-                                <div className="mt-1 text-xl font-bold text-white drop-shadow-md sm:text-2xl">
+                                <div className="mt-1 bg-[linear-gradient(90deg,_#f81a64_0%,_#f52222_50%,_#ff691d_86%,_#ffb623)] bg-clip-text text-xl font-bold text-transparent drop-shadow-md sm:text-2xl">
                                     {result}
                                 </div>
+                                <p className="mt-2 font-medium">
+                                    You're moving forward to the interview
+                                    round! 🌟
+                                </p>
                                 <div className="mt-1 text-xl font-bold text-white drop-shadow-md sm:text-2xl">
                                     Young Webmaster Camp 20
                                 </div>
                             </div>
-                            <div className="mt-4 space-y-3 rounded-md border border-green-500/30 bg-white/5 p-4 text-center text-sm text-white sm:text-base">
+                            <div className="mx-auto mt-4 max-w-3/4 space-y-3 rounded-md border border-green-500/30 bg-white/5 p-4 text-center text-sm text-white sm:max-w-2/3 sm:min-w-fit sm:text-base">
                                 <p>
                                     <span className="font-semibold">
                                         Interview ID:
-                                    </span>
-                                    <br className="sm:hidden" />{' '}
+                                    </span>{' '}
                                     {matched.interviewRefNo}
                                 </p>
                                 <p>
                                     <span className="font-semibold">
                                         Session:
-                                    </span>
-                                    <br className="sm:hidden" /> 1:00 PM – 5:00
-                                    PM
-                                </p>
-                                <p className="mt-2 font-medium text-green-300">
-                                    You're moving forward to the interview
-                                    round! 🌟
+                                    </span>{' '}
+                                    1:00 PM – 5:00 PM
                                 </p>
                             </div>
 
@@ -157,22 +165,22 @@ function Announcement() {
                         </>
                     )}
                 </CardContent>
+                <Button
+                    onClick={() =>
+                        navigate({
+                            to: '/',
+                            search: {
+                                firstName: firstName,
+                                lastName: lastName,
+                                major: major,
+                            }, // Reset the search parameters
+                        })
+                    } // Use navigate to go back
+                    className="self-center bg-[linear-gradient(90deg,_#f81a64_0%,_#f52222_50%,_#ff691d_86%,_#ffb623)] hover:opacity-70 sm:w-1/3"
+                >
+                    Go Back
+                </Button>
             </Card>
-            <Button
-                onClick={() =>
-                    navigate({
-                        to: '/',
-                        search: {
-                            firstName: firstName,
-                            lastName: lastName,
-                            major: major,
-                        }, // Reset the search parameters
-                    })
-                } // Use navigate to go back
-                className="bg-[linear-gradient(90deg,_#f81a64_0%,_#f52222_50%,_#ff691d_86%,_#ffb623)] hover:opacity-70"
-            >
-                Go Back
-            </Button>
         </div>
     );
 }
